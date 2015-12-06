fcb9f2e08b54e2e1824ebbefa4e68bd850a62cd2
AaronHsu219 <civilboy1@gmail.com>
2015-12-06 22:16:12 +0800

    初學者入門作業
    初學者入門作業

---
+++ HW02/hw02Library/Array.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw02Library
+{
+    public class Array
+    {
+        public double[] values;
+
+        //Constructor 建構式:沒有任何回傳型別
+        public Array(int length)
+        {
+            values = new double[length];
+            for (int index = 0; index < length; index++)
+            {
+                values[index] = 0;
+            }
+        }
+
+        // == Functions writen for homework ==
+        public double Sum()
+        {
+            double sum = 0;
+            for (int i = 0; i < values.Length; i++)
+            {
+                sum += values[i];
+            }
+            return sum;
+        }
+
+        public Array Cusum()
+        {
+            double[] table = new double[this.values.Length];
+            double sum = 0;
+            for (int i = 0; i < values.Length; i++)
+            {
+                sum += values[i];
+                table[i] = sum;
+            }
+            this.values = table;
+            return this;
+        }
+
+        public Array Add(double value)
+        {
+            double[] table = new double[this.values.Length];
+            for (int i = 0; i < values.Length; i++)
+            {
+                table[i] = values[i] + value;
+            }
+            this.values = table;
+            return this;
+        }
+
+        public Array Sub(double value)
+        {
+            double[] table = new double[this.values.Length];
+            for (int i = 0; i < values.Length; i++)
+            {
+                table[i] = values[i] - value;
+            }
+            this.values = table;
+            return this;
+        }
+
+        public Array Multiply(double value)
+        {
+            double[] table = new double[this.values.Length];
+            for (int i = 0; i < values.Length; i++)
+            {
+                table[i] = values[i] * value;
+            }
+            this.values = table;
+            return this;
+        }
+
+        public Array Div(double value)
+        {
+            double[] table = new double[this.values.Length];
+            for (int i = 0; i < values.Length; i++)
+            {
+                table[i] = values[i] / value;
+            }
+            this.values = table;
+            return this;
+        }
+
+        // == Functions given by instructor ==
+        public Array Remove(int index)
+        {
+            double[] table = new double[this.values.Length - 1];
+            int count = 0;
+            for (int Index = 0; Index < values.Length; Index++)
+            {
+                if (Index == index)
+                {
+                    continue;
+                }
+                table[count] = values[Index];
+                count++;
+            }
+            this.values = table;
+
+            return this;
+        }
+
+        public void Print()
+        {
+            for (int index = 0; index < this.values.Length; index++)
+            {
+                Console.WriteLine("Array[{0}]={1}", index, values[index]);
+            }
+        }
+
+        public Array GenerateRamdomValues()
+        {
+            Random random = new Random();
+            for (int index = 0; index < this.values.Length; index++)
+            {
+                this.values[index] = random.Next(0, this.values.Length);
+            }
+            return this;
+        }
+
+        public Array GenerateSequence()
+        {
+            for (int index = 0; index < this.values.Length; index++)
+            {
+                this.values[index] = index;
+            }
+            return this;
+        }
+    }
+}
---
+++ HW02/hw02_1/Program.cs
@@ -0,0 +1,47 @@
+﻿using hw02Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw02_1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            // == Generate 0 array
+            hw02Library.Array numbers2 = new hw02Library.Array(10);
+
+            // == TEST Array.GenerateSequence()
+            Console.WriteLine(" <== GenerateSequence() ==>");
+            numbers2.GenerateSequence();
+            numbers2.Print();
+
+            // == TEST Sum()
+            Console.WriteLine("\n <== Test Sum() ==>");
+            Console.WriteLine(numbers2.Sum());
+
+            // == TEST Cusum()
+            Console.WriteLine("\n <== Test Cusum() ==>");
+            numbers2.Cusum().Print();
+
+            // == TEST Add();
+            Console.WriteLine("\n <== Test Add() ==>");
+            numbers2.Add(3).Print();
+
+            // == TEST Sub();
+            Console.WriteLine("\n <== Test Sub() ==>");
+            numbers2.Sub(3).Print();
+
+            // == TEST Multiply();
+            Console.WriteLine("\n <== Test Multify() ==>");
+            numbers2.Multiply(3).Print();
+
+            // == TEST Div();
+            Console.WriteLine("\n <== Test Div() ==>");
+            numbers2.Div(3).Print();
+        }
+    }
+}
